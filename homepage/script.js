5789c54a208ead28042d9903d2e5057b28d7d6e1
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2022-12-14 21:39:42 +0530

    Uploaded the files for the website.
    Uploaded the files for the website.

---
+++ homepage/script.js
@@ -0,0 +1,11 @@
+//Navbar JS
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("#nav");
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("#nav ul li a:hover");
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
