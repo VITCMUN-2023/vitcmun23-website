2c9b63348dbf63709c856b6c4aca463993b61a6a
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2022-12-16 14:14:18 +0530

    Add files via upload

--- homepage/script.js
+++ homepage/script.js
@@ -1,14 +1,21 @@
-//Navbar JS
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        console.log(scroll);
+        if (scroll > 600) {
+            $(".navbar").css("background", "white");
+            $(".nav-a").css("color", "black");
+        }
 
+        else {
+            $(".navbar").css("background", "none");
+            $(".nav-a").css("color", "white");
 
+        }
+    })
+})
 
 window.addEventListener("scroll", function () {
     var header = document.querySelector("#nav");
     header.classList.toggle('sticky', window.scrollY > 0);
 });
-
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("#nav ul li a:hover");
-    header.classList.toggle('sticky', window.scrollY > 0);
-});
-
