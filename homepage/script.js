ada993f109a52d1a76753b0d50f04d1ff5a6d196
Mohammad Shaad Shaikh <mohammadshaadshaikh@Shaad-MacBook-Pro.local>
2022-12-17 23:32:20 +0530

    Made the website responsive

--- homepage/script.js
+++ homepage/script.js
@@ -1,21 +1,27 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        console.log(scroll);
-        if (scroll > 50) {
-            $(".navbar").css("background", "white");
-            $(".nav-a").css("color", "black");
-        }
-
-        else {
-            $(".navbar").css("background", "none");
-            $(".nav-a").css("color", "white");
-
-        }
-    })
-})
-
 window.addEventListener("scroll", function () {
     var header = document.querySelector("#nav");
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+var more = document.querySelector('.more');
+var overlay = document.querySelector('.overlay');
+var cancel = document.querySelector('.cancel');
+var navbar = document.querySelector('.mobile-nav-ele');
+
+more.addEventListener("click", function () {
+    // console.log("clicked");
+
+    overlay.style.display = "block";
+    cancel.style.display = "block";
+    navbar.style.display = "block";
+    more.style.display = "none";
+
+    navbar.addEventListener("click", function () {
+        cancel.style.display = "none";
+        overlay.style.display = "none";
+        navbar.style.display = "none";
+        more.style.display = "block";
+    })
+
+})
+
